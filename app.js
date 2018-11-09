98c9a7f41694c7e353e2f3eddd7dd4635fa7911f
Baugrems <baugrems@swcombine.com>
2018-11-09 02:09:22 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var flash = require("connect-flash");
+var mysql = require("mysql");
+var methodOverride = require("method-override");
+
+// Set main routes
+var indexRoutes = require("./routes/index");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(flash());
+app.use(methodOverride("_method"));
+
+// Gets routes
+app.use(indexRoutes);
+
+// This starts the server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
