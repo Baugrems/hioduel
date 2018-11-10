f9a0b970fb854cd72564e7eccbac0dbf94f433b8
Baugrems <baugrems@swcombine.com>
2018-11-10 02:26:48 -0700

    add db and add new spell page

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ var flash = require("connect-flash");
 var mysql = require("mysql");
 var methodOverride = require("method-override");
 
+var app = express();
+
 // Set main routes
 var indexRoutes = require("./routes/index");
 var spellRoutes = require("./routes/spellroutes");
 
-var app = express();
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(flash());
 app.use(methodOverride("_method"));
+
+var con = mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    //insecureAuth : true
+  });
+
+
+con.query('SELECT * FROM Spells ORDER BY name ASC', function (error, results, fields) {
+    if (error)
+        throw error;
+    results.forEach(result => {
+        console.log(result);
+    });
+});
 
 // Gets routes
 app.use(indexRoutes);
--- routes/spellroutes.js
+++ routes/spellroutes.js
@@ -1,13 +1,40 @@
 var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
 
+router.use(bodyParser.urlencoded({extended: true}));
+
+var con = mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    //insecureAuth : true
+  });
 
 router.get("/", function(req, res){
-    res.render("spells");
+    con.query('SELECT * FROM Spells ORDER BY name ASC', function (error, results, fields) {
+        if (error)
+            throw error;
+        res.render("spells", {results: results});
+    });
+});
+
+router.post("/", function(req, res){
+    con.query(('INSERT INTO Spells (name, damage) VALUES ("' + req.body.name + '", ' + req.body.damage + ')'), function (error, results, fields) {
+        if (error)
+            throw error;
+        res.redirect("/spells");
+    });
+});
+
+router.get("/new", function(req, res){
+    res.render("spellsnew");
 });
 
 router.get("*", function(req, res){
-    req.flash("error", "Spell not found")
+   // req.flash("error", "Spell not found")
     res.redirect("/");
 });
 
